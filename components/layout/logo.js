4e586dba2481f2ce94a87eb789d6cc9caf794811
javad7899 <javadhashemzadeh78@gmail.com>
2024-03-06 08:41:52 +0330

    edit logo

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -1,7 +1,7 @@
 import classes from './logo.module.css';
 
 function Logo() {
-  return <div className={classes.logo}>Max' Next Blog</div>;
+  return <div className={classes.logo}>Next Blog</div>;
 }
 
 export default Logo;
